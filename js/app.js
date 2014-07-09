5b4b35fb31424c41aed8d032d3415b0b90971312
John Varouhakis <johnvarouhakis@gmail.com>
2014-07-10 00:34:46 +0300

    loadscript directive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('hangmanApp', ['ngRoute','ngAnimate']);
+var app = angular.module('hangmanApp', ['ngRoute','ngAnimate','ngLoadScript']);
 
 app.config( function($routeProvider) {
 
