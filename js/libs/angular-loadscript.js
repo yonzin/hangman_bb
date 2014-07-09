b434a75b40780d484310a3921ec0eaaadc99ba4a
John Varouhakis <johnvarouhakis@gmail.com>
2014-07-10 01:05:38 +0300

    adv test

--- js/libs/angular-loadscript.js
+++ js/libs/angular-loadscript.js
@@ -29,29 +29,14 @@
  
   var app = ng.module('ngLoadScript', []);
 
-  app.directive('script', function() {
+  app.directive('adv', function() {
     return {
-      restrict: 'E',
+      restrict: 'A',
       scope: false,
       link: function(scope, elem, attr) 
       {
-        if (attr.type==='text/javascript-lazy') 
-        {
-          var s = document.createElement("script");
-          s.type = "text/javascript";                
-          var src = elem.attr('src');
-          if(src!==undefined)
-          {
-              s.src = src;
-          }
-          else
-          {
-              var code = elem.text();
-              s.text = code;
-          }
-          document.head.appendChild(s);
-          elem.remove();
-        }
+        var adv = $('#leadbolt_adv').children().detach();
+        console.log(adv);
       }
     };
   });
