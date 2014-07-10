7bb56212c624c2433cb003149555e989dccdc004
John Varouhakis <johnvarouhakis@gmail.com>
2014-07-10 03:20:57 +0300

    some ad moving around

--- js/directives.js
+++ js/directives.js
@@ -71,11 +71,17 @@ app.directive("container", function($window) {
   };
 });
 
-app.directive("adscript", function() {
+app.directive("adscript", function($window) {
   return {
     link: function(scope, element, attrs) {
-      var iframe = $('#scripttag iframe').detach();
-      element.append(iframe);
+      var iframe_a = $('#scripttag_a iframe').detach();
+      var iframe_b = $('#scripttag_b iframe').detach();
+      element.append(iframe_a);
+      element.append(iframe_b);
+      iframe_a.css("position","relative");
+      iframe_a.css("top","10px");
+      iframe_b.css("position","relative");
+      iframe_b.css("top","20px");
     },
     restrict: 'A'
   };
